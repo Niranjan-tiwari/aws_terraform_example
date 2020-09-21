3f0dcacbae95c8c602a76cf7fdf0ae93f972234c
war_machine <niranjantiwari667@gmail.com>
2020-09-21 17:13:46 +0530

    terraform basic implementation

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "" # your access_key
-  secret_key = "" #your secret key
+  access_key = "# your access_key"
+  secret_key = "#your secret key"
 }
 
 variable "subnet_prefix" {
